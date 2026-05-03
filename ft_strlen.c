e43f75c51641db0364e3f45a95f0afed884faf22
nephcode@42session <nepcohen@learner.42.tech>
2026-05-03 20:12:47 +0200

    finish practice

--- ft_strlen.c
+++ ft_strlen.c
@@ -6,7 +6,7 @@
 /*   By: nepcohen <nepcohen@learner.42.tech>                /#/      /#/      */
 /*                                                         /#/____  |#| /|    */
 /*   Created: 2026/04/29 17:24:37 by nepcohen             |#######| |#|/#|    */
-/*   Updated: 2026/04/29 20:26:49 by nepcohen         ###   ########.fr       */
+/*   Updated: 2026/05/03 20:06:08 by nepcohen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@ int	ft_strlen(char *str)
 {
 	int count;
 	
+	count = 0;
 	while(str[count] != '\0')
 	{
 		count++;
@@ -24,15 +25,40 @@ int	ft_strlen(char *str)
 /*=== MAIN ===================================================================*/
 
 #include <unistd.h>
+#include <stdio.h>
 
-void display ()
-
-
-
-
-
-int	main(int argc, char argv**)
+static void ft_putchar(char letter)
 {
-	if (argv != 2)
+	write(1, &letter, 1);
+}
 
+static void mininbr(int nbr)
+{
+	if (nbr > 10)
+		mininbr(nbr / 10);
+	ft_putchar((nbr % 10) + '0');
+}
+
+static void display(char *charPlay)
+{
+	int	intPlay;
+
+	intPlay = ft_strlen(charPlay);
+	printf("Lenght = `%d`\n", intPlay);
+	//mininbr(ft_strlen(charPlay));
+}
+
+int	main(int argc, char **argv)
+{
+	char message[] = "Merci de saisir un argument";
+	
+	if (argc == 2)
+	{
+		display(argv[1]);
+	}
+	else
+	{
+		printf("%s", message);
+	}
+	return(0);
 }
