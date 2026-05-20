d51557a50c2ddb315a5fcd4c9a51a5165edfe248
nephcode <nephcohen@gmail.com>
2026-05-20 16:16:43 +0200

    memcmp OK

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -6,25 +6,27 @@
 /*   By: nepcohen <nepcohen@learner.42.tech>                /#/      /#/      */
 /*                                                         /#/____  |#| /|    */
 /*   Created: 2026/05/19 03:07:05 by nepcohen             |#######| |#|/#|    */
-/*   Updated: 2026/05/19 11:05:43 by nepcohen                   |#| NEPH_     */
+/*   Updated: 2026/05/20 16:10:56 by nepcohen                   |#| NEPH_     */
 /*                                                                            */
 /* ************************************************************************** */
 #include <strings.h>
 /* PROG ===================================================================== */
 int	ft_memcmp(const void *s1, const void *s2, size_t n)
 {
-	int				count;
-	unsigned char	*c1;
-	unsigned char	*c2;
+	size_t				count;
+	const unsigned char	*c1;
+	const unsigned char	*c2;
 	
 	c1 = (const unsigned char *)s1;
 	c2 = (const unsigned char *)s2;
-
 	count = 0;
 	while(count < n)
 	{
-		
+		if (c1[count] != c2[count])
+			return (c1[count] - c2[count]);
+		count++;
 	}
+	return (0);
 }
 
 /* MAIN  ==================================================================== */
@@ -42,18 +44,18 @@ void	display(const void *play1, const void *play2, size_t playSize)
 	}
 	else
 	{
-		print("le resultat est `%d`\n", &compare);
+		printf("le resultat est `%d`\n", compare);
 	}
 }
 int	main(int argc, char **argv)
 {
 	if (argc == 4)
 	{
-		display(argv[1], argv[2], atoi(agv[3]);
+		display(argv[1], argv[2], atoi(argv[3]));
 	}
 	else
 	{
-		prinft("merci de saisir trois arguments);
+		printf("merci de saisir trois arguments\n");
 	}
 }
 
