1d46033ad262e284b821e4290410db0b545329d7
nephcode <nephcohen@gmail.com>
2026-05-21 22:50:10 +0200

    memmove

---
+++ ft_memmove.c
@@ -0,0 +1,54 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                             /#/  |#/|#|    */
+/*   ft_memmove.c                                             /#/   |/ |#|    */
+/*                                                           /#/      /#/     */
+/*   By: nepcohen <nepcohen@learner.42.tech>                /#/      /#/      */
+/*                                                         /#/____  |#| /|    */
+/*   Created: 2026/05/21 17:53:17 by nepcohen             |#######| |#|/#|    */
+/*   Updated: 2026/05/21 22:49:10 by nepcohen                   |#| NEPH_     */
+/*                                                                            */
+/* ************************************************************************** */
+include <string.h>
+/* PROG ===================================================================== */
+void *memmove(void *dest, const void *src, size_t n)
+{
+	///
+	///
+	///
+}
+/* MAIN ===================================================================== */
+#include <stdio.h>
+#include <stdlib.h>
+
+void display(void *playDest, const void *playSrc, size_t playN)
+{
+		const unsigned char *result;
+
+		result  = memmove(playDest, playSrc, playN);
+		printf ("",)
+	}
+	else
+	{
+
+	}
+
+}
+
+int	main(int argc, char **argv)
+{
+	if (argc != 4)
+	{
+		printf("Merci de saisir les 3 parametres");
+	}
+	else
+	{
+		
+	}
+
+	return (0);
+}
+
+/* ========================================================================== */
+/* END ================================================== 42_ =============== */
+/* ====================================================== NEPHCODE ========== */
