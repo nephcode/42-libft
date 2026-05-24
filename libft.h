663a48049f378051cdc764c1b01c570ce762a00b
Neph <nephcohen@gmail.com>
2026-05-24 23:53:03 +0200

    fix libft, prototype memcpy

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: nepcohen <nepcohen@learner.42.tech>                /#/      /#/      */
 /*                                                         /#/____  |#| /|    */
 /*   Created: 2026/05/01 21:50:31 by nepcohen             |#######| |#|/#|    */
-/*   Updated: 2026/05/22 20:23:03 by nepcohen                   |#| NEPH_     */
+/*   Updated: 2026/05/24 23:51:48 by nephco                     |#|  NEPH     */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,8 +34,8 @@ int		ft_isdigit(int c)
 int		ft_strlen(char *str)
 void	*ft_memset(void s, int c, size_t n);
 void	*ft_bzero(void *s, size_t n)
-void	ft_memcpy(void dest[], const void src[], size_t n);
-void	*ft_memmove(void dest[], const void src[.n], size_t n);
+void	ft_memcpy(void *dest, const void *src, size_t n);
+void	*ft_memmove(void *dest, const void *src, size_t n);
 size_t	ft_strlcpy(char *dst, const char *src, size_t size);
 size_t	ft_strlcat(char *dst, const char *src, size_t size);
 int		ft_toupper(int c);
