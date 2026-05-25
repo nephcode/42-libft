b23fc8bad4e5422a258cde6d50c3a227758dcdef
nephcode <nephcohen@gmail.com>
2026-05-25 19:25:36 +0200

    workInProgress memset

--- ft_memset.c
+++ ft_memset.c
@@ -6,15 +6,24 @@
 /*   By: nepcohen <nepcohen@learner.42.tech>                /#/      /#/      */
 /*                                                         /#/____  |#| /|    */
 /*   Created: 2026/05/25 14:54:26 by nepcohen             |#######| |#|/#|    */
-/*   Updated: 2026/05/25 15:49:16 by nepcohen                   |#| NEPH_     */
+/*   Updated: 2026/05/25 19:24:48 by nepcohen                   |#| NEPH_     */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft"
 /* PROG ===================================================================== */
 void *ft_memset(void *s, int c, size_t n)
 {
-///
+	size_t count;
+	unsigned char *p;
 
+	p = (unsigned char*)s;
+	count = 0;
+	while  (count < n)
+	{
+		p[count] = (unsigned char)c;
+		count++;
+	}
+	return (s);
 }
 /* MAIN ===================================================================== */
 #include <stdio.h>
@@ -22,10 +31,11 @@ void *ft_memset(void *s, int c, size_t n)
 
 void display(void *playS, int playC, size_t playN)
 {
-	const void result;
-	
+	unsigned 
+	unsigned char *result;
+
+	printf("incremente
 	result = ft_memset(playS, playC, playN);
-	if (result != NULL)
 	{
 		
 	}
@@ -34,7 +44,13 @@ void display(void *playS, int playC, size_t playN)
 
 int main (int argc, char **argv)
 {
-
+	if (argc != 4)
+		printf("Nombre d'argument non valide");
+	else
+	{
+		display(argv[1], argv[2][0], atoi(argv[3]));
+	}
+	return (0);
 }
 /* ========================================================================== */
 /* END =================================================== 42_ ============== */
