5ffa7acc66a9635af3dc0aeaf2ca16bc73f64343
nephcode <nephcohen@gmail.com>
2026-05-26 13:15:27 +0200

    test memset OK

--- ft_memset.c
+++ ft_memset.c
@@ -6,7 +6,7 @@
 /*   By: nepcohen <nepcohen@learner.42.tech>                /#/      /#/      */
 /*                                                         /#/____  |#| /|    */
 /*   Created: 2026/05/25 14:54:26 by nepcohen             |#######| |#|/#|    */
-/*   Updated: 2026/05/25 21:42:00 by nepcohen                   |#| NEPH_     */
+/*   Updated: 2026/05/26 13:06:41 by nepcohen                   |#| NEPH_     */
 /*                                                                            */
 /* ************************************************************************** */
 //#include "libft.h"
@@ -34,19 +34,23 @@ void display(void *playS, int playC, size_t playN)
 {
 	unsigned char *result;
 
+	printf("Avant : `%s`, Incr `%d`\n", (char *)playS, playC);
+	printf("Ram : `%p`, Repeat : `%zu`\n", playS, playN);
 	result = ft_memset(playS, playC, playN);
-	printf("Ch : `%p`\n, Incr : `%d`\n, Rep : `%zu`\n", result, playC, playN);
+	printf("==========================\n");
+	printf("Apres : `%s`\n, Incr : `%d`\n", (char *)result, playC);
+	printf("Ram : `%p`, Repeat : `%zu`\n", result, playN);
 }
 
 int main (int argc, char **argv)
 {
-	char init[] = "ceci est la chaine initiale";
+	//char init[] = "ceci est la chaine initiale";
 	if (argc != 4)
 		printf("Nombre d'argument non valide");
 	else
 	{
-		display(init, argv[2][0], atoi(argv[3]));
-		printf("\n");
+	//	display(init, argv[2][0], atoi(argv[3]));
+	//	printf("\n");
 		display(argv[1], argv[2][0], atoi(argv[3]));
 	}
 	return (0);
