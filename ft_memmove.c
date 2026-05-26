c952ad170fc35d566f2b4381faf4cab1f600e794
nephcode <nephcohen@gmail.com>
2026-05-26 20:36:34 +0200

    memmove debug

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,49 +6,62 @@
 /*   By: nepcohen <nepcohen@learner.42.tech>                /#/      /#/      */
 /*                                                         /#/____  |#| /|    */
 /*   Created: 2026/05/21 17:53:17 by nepcohen             |#######| |#|/#|    */
-/*   Updated: 2026/05/21 22:49:10 by nepcohen                   |#| NEPH_     */
+/*   Updated: 2026/05/26 20:36:09 by nepcohen                   |#| NEPH_     */
 /*                                                                            */
 /* ************************************************************************** */
-include <string.h>
+include <stddef.h>
 /* PROG ===================================================================== */
-void *memmove(void *dest, const void *src, size_t n)
+
+void *ft_memmove(void *dest, const void *src, size_t n)
 {
-	///
-	///
-	///
+	size_t				count;
+	unsigned char		*d;
+	unsigned const char *s;
+
+	d = (unsigned char*)dest;
+	s = (unsigned const char*)src;
+	count = 0;
+	if (dest < src)
+	{
+		while(count < n)
+		{
+			dest[count] = src[count];
+			count++;
+		}
+	else if (dest > src)
+	{
+		while(count < n)
+			dest[count] = src[count];
+			count--;
+	}
+	return(dest));
 }
 /* MAIN ===================================================================== */
+
 #include <stdio.h>
 #include <stdlib.h>
 
-void display(void *playDest, const void *playSrc, size_t playN)
+void display(void *playD, const void *playS, size_t playN)
 {
-		const unsigned char *result;
+        unsigned char *result;
 
-		result  = memmove(playDest, playSrc, playN);
-		printf ("",)
-	}
-	else
-	{
-
-	}
-
+        printf("Avant : `%s`\n Src `%s`\n", (char *)playD, (char *)playS);
+        printf("Ram : `%p`, Replace : `%zu`\n", playD, playN);
+        result = ft_memmove(playD, playS, playN);
+        printf("==========================\n");
+        printf("Apres : `%s`\n Src : `%s`\n", (char *)result, (char *)playS);
+        printf("Ram : `%p`, Replace : `%zu`\n", result, playN);
 }
 
-int	main(int argc, char **argv)
+int     main(int argc, char **argv)
 {
-	if (argc != 4)
-	{
-		printf("Merci de saisir les 3 parametres");
-	}
-	else
-	{
-		
-	}
-
-	return (0);
+        if (argc != 4)
+                printf("Merci de saisir les trois arguments");
+        else
+        {
+                display(argv[1], argv[2], atoi(argv[3]));
+        }
 }
-
 /* ========================================================================== */
 /* END ================================================== 42_ =============== */
 /* ====================================================== NEPHCODE ========== */
