4eb7041cbdc0d96ab6ce8a647939465a0f46120a
nephcode <nephcohen@gmail.com>
2026-05-26 15:40:14 +0200

    memcpy fonction et test OK

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,12 +6,12 @@
 /*   By: nepcohen <nepcohen@learner.42.tech>                /#/      /#/      */
 /*                                                         /#/____  |#| /|    */
 /*   Created: 2026/05/20 15:37:20 by nepcohen             |#######| |#|/#|    */
-/*   Updated: 2026/05/26 14:49:36 by nepcohen                   |#| NEPH_     */
+/*   Updated: 2026/05/26 15:37:20 by nepcohen                   |#| NEPH_     */
 /*                                                                            */
 /* ************************************************************************** */
 #include <stddef.h>
 /* PROG ===================================================================== */
-void	ft_memcpy(void *dest, const void *src, size_t n)
+void	*ft_memcpy(void *dest, const void *src, size_t n)
 {
 	size_t				count;
 	unsigned char		*d;
@@ -29,9 +29,29 @@ void	ft_memcpy(void *dest, const void *src, size_t n)
 }
 
 /* MAIN ===================================================================== */
+#include <stdio.h>
+#include <stdlib.h>
+
+void display(void *playD, const void *playS, size_t playN)
+{
+        unsigned char *result;
+
+        printf("Avant : `%s`\n Src `%s`\n", (char *)playD, (char *)playS);
+        printf("Ram : `%p`, Replace : `%zu`\n", playD, playN);
+        result = ft_memcpy(playD, playS, playN);
+        printf("==========================\n");
+        printf("Apres : `%s`\n Src : `%s`\n", (char *)result, (char *)playS);
+        printf("Ram : `%p`, Replace : `%zu`\n", result, playN);
+}
+
 int	main(int argc, char **argv)
 {
-
+	if (argc != 4)
+		printf("Merci de saisir les trois arguments");
+	else
+	{
+		display(argv[1], argv[2], atoi(argv[3]));
+	}
 }
 
 
