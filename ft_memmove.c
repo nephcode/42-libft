fd46d208210102fe2e7164fad1f037211cafe173
nephcode <nephcohen@gmail.com>
2026-05-26 21:32:03 +0200

    norminette OK memmove

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,17 +6,17 @@
 /*   By: nepcohen <nepcohen@learner.42.tech>                /#/      /#/      */
 /*                                                         /#/____  |#| /|    */
 /*   Created: 2026/05/21 17:53:17 by nepcohen             |#######| |#|/#|    */
-/*   Updated: 2026/05/26 21:18:41 by nepcohen                   |#| NEPH_     */
+/*   Updated: 2026/05/26 21:28:18 by nepcohen                   |#| NEPH_     */
 /*                                                                            */
 /* ************************************************************************** */
 #include <stddef.h>
 /* PROG ===================================================================== */
 static void	avant(unsigned char *d, unsigned const char *s, size_t n)
 {
-	size_t count;
+	size_t	count;
 
 	count = 0;
-	while(count < n)
+	while (count < n)
 	{
 		d[count] = s[count];
 		count++;
@@ -28,20 +28,20 @@ static void	arriere(unsigned char *d, unsigned const char *s, size_t n)
 	size_t	count;
 
 	count = n;
-	while(n > 0)
+	while (n > 0)
 	{
 		count--;
 		d[count] = s[count];
 	}
 }
 
-void *ft_memmove(void *dest, const void *src, size_t n)
+voi	*ft_memmove(void *dest, const void *src, size_t n)
 {
 	unsigned char		*d;
-	unsigned const char *s;
+	unsigned const char	*s;
 
-	d = (unsigned char*)dest;
-	s = (unsigned const char*)src;
+	d = (unsigned char *)dest;
+	s = (unsigned const char *)src;
 	if (dest < src)
 		avant(d, s, n);
 	else if (dest > src)
@@ -55,26 +55,26 @@ void *ft_memmove(void *dest, const void *src, size_t n)
 #include <stdio.h>
 #include <stdlib.h>
 
-void display(void *playD, const void *playS, size_t playN)
+void	display(void *playD, const void *playS, size_t playN)
 {
-        unsigned char *result;
+	unsigned char	*result;
 
-        printf("Avant : `%s`\n Src `%s`\n", (char *)playD, (char *)playS);
-        printf("Ram : `%p`, Replace : `%zu`\n", playD, playN);
-        result = ft_memmove(playD, playS, playN);
-        printf("==========================\n");
-        printf("Apres : `%s`\n Src : `%s`\n", (char *)result, (char *)playS);
-        printf("Ram : `%p`, Replace : `%zu`\n", result, playN);
+	printf("Avant : `%s`\n Src `%s`\n", (char *)playD, (char *)playS);
+	printf("Ram : `%p`, Replace : `%zu`\n", playD, playN);
+	result = ft_memmove(playD, playS, playN);
+	printf("==========================\n");
+	printf("Apres : `%s`\n Src : `%s`\n", (char *)result, (char *)playS);
+	printf("Ram : `%p`, Replace : `%zu`\n", result, playN);
 }
 
-int     main(int argc, char **argv)
+int	main(int argc, char **argv)
 {
-        if (argc != 4)
-                printf("Merci de saisir les trois arguments");
-        else
-        {
-                display(argv[1], argv[2], atoi(argv[3]));
-        }
+	if (argc != 4)
+		printf("Merci de saisir les trois arguments");
+	else
+	{
+		display(argv[1], argv[2], atoi(argv[3]));
+	}
 }
 /* ========================================================================== */
 /* END ================================================== 42_ =============== */
