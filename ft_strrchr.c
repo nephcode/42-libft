4c74df9adedb78b85207df7949e854a22803ff75
Neph <nephcohen@gmail.com>
2026-05-29 04:45:00 +0200

    strchr >> Norminette OK

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -6,7 +6,7 @@
 /*   By: nepcohen <nepcohen@learner.42.tech>                /#/      /#/      */
 /*                                                         /#/____  |#| /|    */
 /*   Created: 2026/05/27 21:14:50 by nepcohen             |#######| |#|/#|    */
-/*   Updated: 2026/05/29 04:37:21 by nephco                     |#|  NEPH     */
+/*   Updated: 2026/05/29 04:44:26 by nephco                     |#|  NEPH     */
 /*                                                                            */
 /* ************************************************************************** */
 #include <stddef.h>
@@ -16,16 +16,17 @@ static int	len(char *chain)
 	int	count;
 
 	count = 0;
-	while(chain[count] != '\0')
+	while (chain[count] != '\0')
 	{
 		count++;
 	}
 	return (count);
 }
 // ----------------------------------------------------------------------------
+
 char	*strrchr(const char *s, int c)
 {
-	size_t	count; 
+	size_t	count;
 
 	count = len((char *)s);
 	while (count > 0)
@@ -38,26 +39,26 @@ char	*strrchr(const char *s, int c)
 	}
 	if (s[0] == (char)c)
 	{
-
 		return ((char *)&s[count]);
 	}
 	return (NULL);
 }
+
 /* MAIN ===================================================================== */
 #include <stdlib.h>
 #include <stdio.h>
 #include <stddef.h>
 
-void    display(const char *playS, int playC)
+void	display(const char *playS, int playC)
 {
-	const char  *result;
+	const char	*result;
 
 	printf("Search `%d` in `%p`\n", playC, playS);
 	result = strrchr(playS, playC);
 	printf("Memory location is `%p`\n", result);
 }
 
-int main(int argc, char **argv)
+int	main(int argc, char **argv)
 {
 	if (argc != 3)
 	{
