ef0aec222483ba4284011279ee949e1d3469dfc0
Neph <nephcohen@gmail.com>
2026-05-30 04:14:11 +0200

    fix bzero work in progress

--- ft_bzero.c
+++ ft_bzero.c
@@ -6,13 +6,13 @@
 /*   By: nepcohen <nepcohen@learner.42.tech>                /#/      /#/      */
 /*                                                         /#/____  |#| /|    */
 /*   Created: 2026/05/13 18:49:13 by nepcohen             |#######| |#|/#|    */
-/*   Updated: 2026/05/15 23:24:48 by nepcohen                   |#| NEPH_     */
+/*   Updated: 2026/05/30 04:12:49 by nephco                     |#|  NEPH     */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft" // include define header and external lib declaration
 
 /* PROG ===================================================================== */
-void	*ft_bzero(void *s, size_t n)
+void	ft_bzero(void *s, size_t n)
 {
 	unsigned char *octet;
 	size_t	count;
@@ -27,10 +27,28 @@ void	*ft_bzero(void *s, size_t n)
 
 /* MAIN ===================================================================== */
 #include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
 
-void	display()
+void	display(void playS, size_t playN)
+{
+	printf("\n", playS, playN);
+	ft_bzero(playS, playN);
+	printf();
 
-int	main(int argc, char **argc
+
+}
+
+int	main(int argc, char **argv)
+{
+	if (argc != 3)
+		printf("merci de saisir les deux Arguments");
+	else
+	{
+		display(argv[1], atoi(argv[2]));
+	}
+	return (0);
+}
 
 /* ========================================================== 42_ =========== */
 /* END  ===================================================== NEPHCODE ====== */
