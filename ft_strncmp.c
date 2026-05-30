54c3ce2f5b9e4c876da08fc31c3d912018e09eae
Neph <nephcohen@gmail.com>
2026-05-30 03:20:33 +0200

    strncmp OK, Test, Norminette

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,42 +6,41 @@
 /*   By: nepcohen <nepcohen@learner.42.tech>                /#/      /#/      */
 /*                                                         /#/____  |#| /|    */
 /*   Created: 2026/05/29 17:57:34 by nepcohen             |#######| |#|/#|    */
-/*   Updated: 2026/05/29 20:00:18 by nepcohen                   |#| NEPH_     */
+/*   Updated: 2026/05/30 03:19:28 by nephco                     |#|  NEPH     */
 /*                                                                            */
 /* ************************************************************************** */
 #include <stddef.h>
 /* PROG ===================================================================== */
 int	strncmp(const char *s1, const char *s2, size_t n)
 {
-	size_t				count;
-	//	unsigned const char	*r1;
-	//unsigned const char	*r2;
-	
+	size_t	count;
+
 	count = 0;
 	if (n != 0)
 	{
-		while((const char)s1[count] != '\0')
+		while (s1[count] != '\0')
 		{
-			if ((const char)s1[count] != (const char)s2[count])
-				return ((const char)s1[count] - (const char)s2[count]);
+			if (s1[count] != s2[count])
+				return ((unsigned char)s1[count] - (unsigned char)s2[count]);
+			if (s1[count] == '\0')
+				return (0);
+			count++;
 		}
-		count++;
 	}
-	else
-		return (0);
+	return (0);
 }
 
 /* MAIN ===================================================================== */
 #include <stdio.h>
 #include <stdlib.h>
 
-void	display(const char playS1, const char *playS2, size_t playN)
+void	display(const char *playS1, const char *playS2, size_t playN)
 {
 	int	result;
-	
-	printf("");
-	printf();
-	printf();
+
+	printf("S1 :`%s`\n", playS1);
+	printf("S2 :`%s`\n", playS2);
+	printf("Size :`%zu`\n", playN);
 	result = strncmp(playS1, playS2, playN);
 	printf("Le result est `%d`\n", result);
 }
@@ -52,12 +51,10 @@ int	main(int argc, char **argv)
 		printf("Merci de saisir les 3 Arguments");
 	else
 	{
-		display(argv[1], argv[2], atoi(argv[3][0]);
+		display(argv[1], argv[2], atoi(argv[3]));
 	}
 	return (0);
 }
-
-
 /* ========================================================================== */
 /* END ============================================================= 42_ ==== */
 /* ================================================================= NEPHCODE */
