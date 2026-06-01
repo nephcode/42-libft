ff329a3f2b09e8704b1ba1b16f153409adfb3239
nephcode <nephcohen@gmail.com>
2026-06-02 00:00:46 +0200

    strstr ok test ok

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,37 +6,37 @@
 /*   By: nepcohen <nepcohen@learner.42.tech>                /#/      /#/      */
 /*                                                         /#/____  |#| /|    */
 /*   Created: 2026/05/31 18:37:23 by nepcohen             |#######| |#|/#|    */
-/*   Updated: 2026/06/01 21:50:48 by nepcohen                   |#| NEPH_     */
+/*   Updated: 2026/06/01 23:58:48 by nepcohen                   |#| NEPH_     */
 /*                                                                            */
 /* ************************************************************************** */
-
+#include <stddef.h>
 /* PROG  ==================================================================== */
 char	*ft_strnstr(const char *big, const char *little, size_t len)
 {
-	size_t count;
-	size_t slot;
+	size_t	count;
+	size_t	slot;
 
 	count = 0;
-	if (*little == '\n')
+	if (*little == '\0')
 		return ((char *)big);
 	if (!len)
-		return (null);
+		return (NULL);
 	else
 	{
-		while((char *)big != '\0' && count < len)
+		while (big[count] != '\0' && count < len)
 		{
 			slot = 0;
-			while (big[count + slot] = little[slot]
+			while (big[count + slot] == little[slot]
 				&& little[slot] != '\0'
 				&& count + slot < len)
-				slot++
+				slot++;
 			if (little[slot] == '\0')
 				return ((char *)&big[count]);
 			count++;
 		}
-	return (NULL);
+		return (NULL);
+	}
 }
-
 /* MAIN ===================================================================== */
 #include <stdio.h>
 #include <stdlib.h>
@@ -44,17 +44,21 @@ char	*ft_strnstr(const char *big, const char *little, size_t len)
 
 void	display(const char *playB, const char *playL, size_t playN)
 {
-	const char result;
+	const char	*result;
 
 	printf("Big : `%s`\n", playB);
 	printf("Little : `%s`\n", playL);
 	printf("Taille : `%zu`\n", playN);
 	result = ft_strnstr(playB, playL, playN);
-	if (result == 0)
-		printf ("Len 0 > Return :`%s`\n", playB);
-	if (
-		printf("Rien"
-	if (
+	if (result == NULL)
+		printf("NULL\n");
+	else if (result == playB)
+		printf("Little vide : `%s`\n", result);
+	else
+	{
+		printf("Chaine : `%s`\n", result);
+		printf("Pointer : `%p`\n", (void *)result);
+	}
 }
 
 int	main(int argc, char **argv)
@@ -63,7 +67,7 @@ int	main(int argc, char **argv)
 		printf("Merci de saisir les 3 arguments du main");
 	else
 	{
-		display(argv[1], argv[2], atoi(arg[3]));
+		display(argv[1], argv[2], atoi(argv[3]));
 	}
 	return (0);
 }
