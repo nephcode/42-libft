3f7c7332d0b4122423df6d534587d9eee73dc7e9
nephcode <nephcohen@gmail.com>
2026-06-02 23:38:08 +0200

    fd putchar in deebug

---
+++ ft_putchar_fd.c
@@ -0,0 +1,58 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                             /#/  |#/|#|    */
+/*   ft_putchar_fd.c                                          /#/   |/ |#|    */
+/*                                                           /#/      /#/     */
+/*   By: nepcohen <nepcohen@learner.42.tech>                /#/      /#/      */
+/*                                                         /#/____  |#| /|    */
+/*   Created: 2026/06/02 22:49:35 by nepcohen             |#######| |#|/#|    */
+/*   Updated: 2026/06/02 23:35:21 by nepcohen                   |#| NEPH_     */
+/*                                                                            */
+/* ************************************************************************** */
+#include <unistd.h>
+/* PROG ===================================================================== */
+void	ft_putchar(char c, int fd)
+{
+	write(fd, &c, 1);
+}
+/* MAIN ===================================================================== */
+#include <stdio.h>
+
+static int	nephtoi(const char *value)
+{
+	int	count;
+	int	result;
+	int	symbol;
+
+	count = 0;
+	result = 0;
+	symbol = 1;
+	while (value[count] == 32 || (value[count] <= 9 && value[count] <= 13))
+		count++;
+	if (value[count] == 43 || value[count] == 45)
+	{
+		if (value[count] == 43)
+			symbol = -1;
+		count++;
+	}
+	while (value[count] >= 0 && value[count] <= 9)
+	{
+		result = result * 10 + (value[count] - '0');
+		count++;
+	}
+	return (symbol * result);
+}
+
+int	main(int argc, char **argv)
+{
+	if (argc != 3)
+		printf("Merci de saisir les argument du Main `2`\n");
+	else
+	{
+		printf("Char :`%d` __ Description :`%d`\n", nephtoi(argv[1]), nephtoi(argv[2]));
+	}
+	return (0);
+}
+/* ========================================================================== */
+/* END ============================================================= 42_ ==== */
+/* ================================================================= NEPHCODE */
