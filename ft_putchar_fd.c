e2eaca4813ec91f8b90271087c65b934d2fa72af
Neph <nephcohen@gmail.com>
2026-06-03 02:18:14 +0200

    ft_putchar_fd fixed, test OK

--- ft_putchar_fd.c
+++ ft_putchar_fd.c
@@ -6,12 +6,12 @@
 /*   By: nepcohen <nepcohen@learner.42.tech>                /#/      /#/      */
 /*                                                         /#/____  |#| /|    */
 /*   Created: 2026/06/02 22:49:35 by nepcohen             |#######| |#|/#|    */
-/*   Updated: 2026/06/02 23:35:21 by nepcohen                   |#| NEPH_     */
+/*   Updated: 2026/06/03 02:15:26 by nephco                     |#|  NEPH     */
 /*                                                                            */
 /* ************************************************************************** */
 #include <unistd.h>
 /* PROG ===================================================================== */
-void	ft_putchar(char c, int fd)
+void	ft_putchar_fd(char c, int fd)
 {
 	write(fd, &c, 1);
 }
@@ -29,18 +29,24 @@ static int	nephtoi(const char *value)
 	symbol = 1;
 	while (value[count] == 32 || (value[count] <= 9 && value[count] <= 13))
 		count++;
-	if (value[count] == 43 || value[count] == 45)
+	if (value[count] == '-' || value[count] == '+')
 	{
-		if (value[count] == 43)
+		if (value[count] == '-')
 			symbol = -1;
 		count++;
 	}
-	while (value[count] >= 0 && value[count] <= 9)
+	while (value[count] >= '0' && value[count] <= '9')
 	{
 		result = result * 10 + (value[count] - '0');
 		count++;
 	}
 	return (symbol * result);
+}
+
+static void	display(char play, int num)
+{
+	ft_putchar_fd(play, num);
+	ft_putchar_fd('\n', 1);
 }
 
 int	main(int argc, char **argv)
@@ -49,7 +55,8 @@ int	main(int argc, char **argv)
 		printf("Merci de saisir les argument du Main `2`\n");
 	else
 	{
-		printf("Char :`%d` __ Description :`%d`\n", nephtoi(argv[1]), nephtoi(argv[2]));
+		//printf("Char :`%d` __ Description :`%d`\n", nephtoi(argv[1]), nephtoi(argv[2]));
+		display(argv[1][0], nephtoi(argv[2]));
 	}
 	return (0);
 }
