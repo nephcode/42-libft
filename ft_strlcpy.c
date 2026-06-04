c5cb1500a71804068d335213723fbb400f0e29ed
nephcode <nephcohen@gmail.com>
2026-06-04 22:42:39 +0200

    strlcpy Finished, test OK

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -6,41 +6,39 @@
 /*   By: nepcohen <nepcohen@learner.42.tech>                /#/      /#/      */
 /*                                                         /#/____  |#| /|    */
 /*   Created: 2026/06/04 17:42:59 by nepcohen             |#######| |#|/#|    */
-/*   Updated: 2026/06/04 21:15:58 by nepcohen                   |#| NEPH_     */
+/*   Updated: 2026/06/04 22:40:54 by nepcohen                   |#| NEPH_     */
 /*                                                                            */
 /* ************************************************************************** */
 #include <stddef.h>
 /* PROG ===================================================================== */
-static size_t len(char *value)
+static size_t	len(const char *value)
 {
-	char	*lenght;
+	const char	*length;
 
-	lenght = value;
-	while(*value)
+	length = value;
+	while (*value)
 		value++;
-	return(value - lenght);
+	return (value - length);
 }
 
-void	ft_strlcpy(char *dst, const char *src, size_t size)
+size_t	ft_strlcpy(char *dst, const char *src, size_t size)
 {
-	size_t count;
-	size_t i;
-	
+	size_t	length;
+	size_t	i;
+
 	i = 0;
-	count = len(src);
+	length = len(src);
 	if (size == 0)
-		return (count);
+		return (length);
 	else
 	{
-		while(src[i] != '\0')
+		while (src[i] != '\0' && i < size -1)
 		{
-			dst[i] = s
+			dst[i] = src[i];
 			i++;
-			if (i == count)
-			{
-				dst[1] = '\0';
-			}
 		}
+		dst[i] = '\0';
+		return (length);
 	}
 }
 /* MAIN ===================================================================== */
@@ -71,20 +69,23 @@ static int	nephtoi(char *num)
 	return (symbol * result);
 }
 
-static void display(char *playD, const char playS, size_t playI)
+static void	display(char *playD, const char *playS, size_t playI)
 {
+	size_t	result;
 
+	result = ft_strlcpy(playD, playS, playI);
+	printf("Return: `%zu`\n", result);
+	printf("Dest: `%s`\n", playD);
 }
 
-int	main (int argc, char ** argv)
+int	main(int argc, char **argv)
 {
-	if (argc != 4)
-		printf("Merci de saisir les 3 arguments du main");
-	else
+	if (argc != 4 )
 	{
-
+		printf("Merci de saisir les 3 arguments du main");
+		return (1);
 	}
-
+	display(argv[1], argv[2], nephtoi(argv[3]));
 	return (0);
 }
 /* ========================================================================== */
