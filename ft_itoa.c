4f0bff2027d595d177610eccf0e6ef32f53aa253
nephcode <nephcohen@gmail.com>
2026-06-12 22:04:05 +0200

    itoa in prgress

---
+++ ft_itoa.c
@@ -0,0 +1,71 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                             /#/  |#/|#|    */
+/*   ft_itoa.c                                                /#/   |/ |#|    */
+/*                                                           /#/      /#/     */
+/*   By: nepcohen <nepcohen@learner.42.tech>                /#/      /#/      */
+/*                                                         /#/____  |#| /|    */
+/*   Created: 2026/06/12 21:24:45 by nepcohen             |#######| |#|/#|    */
+/*   Updated: 2026/06/12 21:59:12 by nepcohen                   |#| NEPH_     */
+/*                                                                            */
+/* ************************************************************************** */
+
+/* PROG ===================================================================== */
+char	*ft_itoa(int n)
+{
+	//while space
+	
+	//if symbol
+	
+	//while number  
+	// putmbn
+	// return ft_subtr
+
+}
+/* MAIN ===================================================================== */
+static int	nephtoi(const char *nptr)
+{
+        int     count;
+        int     result;
+        int     symbol;
+
+        count = 0;
+        result = 0;
+        symbol = 1;
+        while (nptr[count] == 32 || (nptr[count] >= 9 && nptr[count] <= 13))
+                count++;
+        if (nptr[count] == '-' || nptr[count] == '+')
+        {
+                if (nptr[count] == '-')
+                        symbol = -1;
+                count++;
+        }
+        while (nptr[count] >= '0' && nptr[count] <= '9')
+        {
+                result = result * 10 + (nptr[count] - '0');
+                count++;
+        }
+        return (symbol * result);
+}
+
+
+static void	display()
+{
+
+}
+
+// -------------------------------------------------------------------------- //
+int	main(int argc, **argv)
+{
+	if (argc != 2)
+	{
+		printf("Merci de saisir les arguments du main");
+		return (1)
+	}
+	display(nephtoi(argv[1]));
+}
+
+
+/* ========================================================================== */
+/* END ============================================================= 42_ ==== */
+/* ================================================================= NEPHCODE */
